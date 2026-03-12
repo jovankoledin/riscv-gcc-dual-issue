34a0ac280a0df02bc9861e3a76cef4833e0be56a
Jovan T Koledin <jovank@cs.washington.edu@kiska.cs.washington.edu>
2026-03-12 10:44:13 -0700

    Initial changes

--- gcc/config/riscv/riscv.c
+++ gcc/config/riscv/riscv.c
@@ -320,7 +320,7 @@ static const struct riscv_tune_info bsg_vanilla_2020_tune_info = {
   {COSTS_N_INSNS (20), COSTS_N_INSNS (20)},	/* fp_div */
   {COSTS_N_INSNS (4), COSTS_N_INSNS (4)},	/* int_mul */
   {COSTS_N_INSNS (6), COSTS_N_INSNS (6)},	/* int_div */
-  1,						/* issue_rate */
+  2,						/* issue_rate */
   3,						/* branch_cost */
   5,						/* memory_cost */
   true,						/* slow_unaligned_access */
@@ -456,6 +456,20 @@ riscv_build_integer_1 (struct riscv_integer_op codes[RISCV_MAX_INTEGER_OPS],
 
   gcc_assert (cost <= RISCV_MAX_INTEGER_OPS);
   return cost;
+}
+
+static int
+riscv_variable_issue (FILE *file, int verbose, rtx_insn *insn, int more)
+{
+  /* If the instruction we just scheduled is a branch/jump,
+     we want to prevent any more instructions from issuing this cycle. */
+  if (get_attr_type (insn) == TYPE_BRANCH
+      || get_attr_type (insn) == TYPE_JUMP
+      || get_attr_type (insn) == TYPE_CALL)
+    return 0; /* No more instructions can issue this cycle */
+
+  /* Otherwise, just decrement the issue slots normally */
+  return more - 1;
 }
 
 /* Fill CODES with a sequence of rtl operations to load VALUE.
